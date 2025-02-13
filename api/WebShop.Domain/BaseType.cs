d4b3e669a2b6a31c320c7f9eac774c465e24e586
mijat96 <marko_srb@hotmail.rs>
2025-02-13 21:29:14 +0100

    add base type

---
+++ api/WebShop.Domain/BaseType.cs
@@ -0,0 +1,10 @@
+﻿namespace WebShop.Domain
+{
+    public abstract class BaseType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        //public Guid CreatorId { get; set; }
+        public DateTime CreationTimestamp { get; set; }
+    }
+}
--- api/WebShop/Program.cs
+++ api/WebShop/Program.cs
@@ -6,12 +6,20 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+// Add swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    // Add swagger
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
 }
 
 app.UseHttpsRedirection();
