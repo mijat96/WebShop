ccef942970bf18c92d8a6d9840e75aeb2ec7d952
mijat96 <marko_srb@hotmail.rs>
2025-02-24 21:06:20 +0100

    install entityFramework, add dbSet

---
+++ api/WebShop.Infrastructure/WebShopDbContext.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using WebShop.Domain.Models;
+
+namespace WebShop.Infrastructure
+{
+    public class WebShopDbContext(DbContextOptions<WebShopDbContext> options) : DbContext(options)
+    {
+        public DbSet<Company> Company => Set<Company>();
+        public DbSet<Group> Group => Set<Group>();
+        public DbSet<Item> Item => Set<Item>();
+        public DbSet<Order> Order => Set<Order>();
+        public DbSet<OrderItem> OrderItem => Set<OrderItem>();
+        public DbSet<User> User => Set<User>();
+    }
+}
