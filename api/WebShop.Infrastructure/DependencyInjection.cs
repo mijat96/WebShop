c73b2d23937137bef52f31c1f5c9e1486da369fd
mijat96 <marko_srb@hotmail.rs>
2025-02-24 21:34:34 +0100

    add dependencyInjection and dbContext

---
+++ api/WebShop.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WebShop.Domain.Models;
+
+namespace WebShop.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection RegisterSqlDataAccess(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<WebShopDbContext>(builder =>
+            {
+                var connectionString = configuration["ConnectionStrings:DefaultConnection"];
+                builder.UseSqlServer(connectionString);
+            });
+
+            return RegisterRepositories(services);
+        }
+
+        private static IServiceCollection RegisterRepositories(this IServiceCollection services)
+        {
+            //services.AddScoped<,>();
+
+            return services;
+        }
+    }
+}
--- api/WebShop/Program.cs
+++ api/WebShop/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.Extensions.Configuration;
+using WebShop.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +8,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+// Add DbContext and register repositories
+DependencyInjection.RegisterSqlDataAccess(builder.Services, builder.Configuration);
 
 // Add swagger
 builder.Services.AddEndpointsApiExplorer();
