27d3776e6af3b432882f2e561b2e5566fbfdf897
mijat96 <marko_srb@hotmail.rs>
2025-03-18 21:18:13 +0100

    add IbaseRepository and implementation

---
+++ api/Services.Interfaces/IRepositories/IBaseRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Services.Interfaces.IRepositories
+{
+    public interface IBaseRepository<T>
+    {
+        Task<T> Create(T newItem);
+        Task<IEnumerable<T>> Search(string searchTerm, int numberOfResults);
+        Task<T> GetSingle(int id);
+    }
+}
---
+++ api/WebShop.Infrastructure/Repositories/BaseRepository.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Services.Interfaces.IRepositories;
+using WebShop.Domain;
+
+namespace WebShop.Infrastructure.Repositories
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : BaseType
+    {
+        public WebShopDbContext _context { get; set; }
+
+        protected DbSet<T> Items => _context.Set<T>();
+
+        public BaseRepository(WebShopDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<T> Create(T item)
+        {
+            await Items.AddAsync(item);
+            await _context.SaveChangesAsync();
+
+            return item;
+        }
+
+        public async Task<T> GetSingle(int id)
+        {
+            return await Items.FirstAsync(x => x.Id == id);
+        }
+
+        public async Task<IEnumerable<T>> Search(string searchTerm, int numberOfResults)
+        {
+            var result = await Items
+                    .Where(item => item.Name.Contains(searchTerm ?? ""))
+                    .OrderBy(item => item.CreationTimestamp)
+                    .Take(numberOfResults).ToListAsync();
+
+            return result;
+        }
+    }
+}
