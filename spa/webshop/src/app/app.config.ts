344a0a609838e71091ce7572c12a2d4a6a566fb9
milospesic <milos.pesic@rationaletech.com>
2026-02-17 11:02:56 +0100

    adding styles

--- spa/webshop/src/app/app.config.ts
+++ spa/webshop/src/app/app.config.ts
@@ -1,5 +1,12 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
 
 import { routes } from './app.routes';
 
@@ -7,6 +14,13 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    // providePrimeNG({
+    //   ripple: true,
+    //   theme: {
+    //     preset: Aura,
+    //   },
+    // }),
+  ],
 };
